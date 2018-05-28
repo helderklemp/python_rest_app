b76e8beac4db693c75127bdfe596627d14b631e9
Helder Klemp <helder.klemp@cmdsolutions.com.au>
2018-05-28 16:56:42 +1000

    fix flask hostname

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Blueprint
 from flask_restful import Api
 from resources.Hello import Hello
+from resources.Health import Health
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
 
 # Route
-api.add_resource(Hello, '/Hello')
+api.add_resource(Hello, '/')
+api.add_resource(Health, '/health')
+
